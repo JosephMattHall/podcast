9c593295e04231a5866916962b406417643f4e95
LoveToLearn <hollyburton39@gmail.com>
2023-04-02 23:57:37 +0000

    Update 2 files

--- auth/AuthUserContext.js
+++ auth/AuthUserContext.js
@@ -1,4 +1,4 @@
-import { createContext, useContext, Context } from "react";
+import { createContext, useContext } from "react";
 import useFirebaseAuth from "./useFirebaseAuth";
 
 const authUserContext = createContext({
