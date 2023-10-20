f6069c8dee30a0f4379170a5f884f551bcbddfb3
Joseph Hall <121320782+JosephMattHall@users.noreply.github.com>
2023-10-20 16:35:28 -0400

    fixed delete button

--- pages/admin/episodes/index.jsx
+++ pages/admin/episodes/index.jsx
@@ -1,9 +1,12 @@
 // pages/admin/index.jsx
 import { useEffect, useState } from 'react';
-import { collection, getDocs } from 'firebase/firestore';
+import { collection, getDocs, deleteDoc, doc } from 'firebase/firestore';
 import { db } from '@/firebase-config';
-import { Box, Container, Typography, Button, Grid } from '@mui/material';
+import { Container, Typography, Button, Grid, TableContainer, Table, TableHead, TableBody, TableRow, TableCell, IconButton, Paper } from '@mui/material';
 import Link from 'next/link';
+import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit';
+
 
 export default function AdminIndex() {
   const [loading, setLoading] = useState(false);
@@ -21,40 +24,58 @@ export default function AdminIndex() {
     fetchEpisodes();
   }, []);
 
+  const handleDelete = async (id) => {
+    try {
+      await deleteDoc(doc(db, 'episodes', id));
+      setEpisodes((prevEpisodes) => prevEpisodes.filter((episode) => episode.id !== id));
+    } catch (error) {
+      console.error('Error deleting episode:', error);
+    }
+  };
+
   return (
-    <Container>
-      <Typography variant="h4" gutterBottom>
-        Admin Dashboard
-      </Typography>
-      <Grid container justifyContent="flex-end">
+    <Container sx={{ mt: 4 }}>
+      <Grid container justifyContent="space-between" alignItems="center" marginBottom={4}>
+        <Typography variant="h4">Dashboard / Episodes</Typography>
         <Link href="/admin/episodes/create" passHref>
-          <Button variant="contained" color="primary" sx={{ marginBottom: 2 }}>
+          <Button variant="contained" color="primary">
             New Episode
           </Button>
         </Link>
       </Grid>
       {loading && <p>Loading...</p>}
       {episodes.length > 0 && (
-        <Box>
-          <Typography variant="h6" gutterBottom>
-            Episodes
-          </Typography>
-          {episodes.map((episode) => (
-            <Box key={episode.id} sx={{ marginBottom: 2 }}>
-              <Typography variant="subtitle1">{episode.title}</Typography>
-              <Link href={`/admin/episodes/${episode.id}`} passHref>
-                <Button variant="outlined" color="primary" sx={{ marginRight: 2 }}>
-                  View
-                </Button>
-              </Link>
-              <Link href={`/admin/episodes/${episode.id}/edit`} passHref>
-                <Button variant="outlined" color="secondary">
-                  Edit
-                </Button>
-              </Link>
-            </Box>
-          ))}
-        </Box>
+        <TableContainer component={Paper}>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>Title</TableCell>
+                <TableCell align="right">Season</TableCell>
+                <TableCell align="right">Episode</TableCell>
+                <TableCell align="right">Actions</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {episodes.map((episode) => (
+                <TableRow key={episode.id}>
+                  <TableCell>{episode.title}</TableCell>
+                  <TableCell align="right">{episode.season}</TableCell>
+                  <TableCell align="right">{episode.episode}</TableCell>
+                  <TableCell align="right">
+                    <Link href={`/admin/episodes/${episode.id}`} passHref>
+                      <IconButton color="primary">
+                        <EditIcon />
+                      </IconButton>
+                    </Link>
+                    <IconButton color="secondary" onClick={() => handleDelete(episode.id)}>
+                      <DeleteIcon />
+                    </IconButton>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
       )}
     </Container>
   );
